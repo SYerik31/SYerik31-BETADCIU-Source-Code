0038defecd918e2dca83c26d87aec22528574291
SYerik31 <100543116+SYerik31@users.noreply.github.com>
2022-03-05 15:39:43 -0500

    Songs Data

---
+++ assets/preload/data/dream-of-peace/modchart.lua
@@ -0,0 +1,28 @@
+function start (song)
+
+       changeDadCharacter('eteled', 60, 300)
+       changeBoyfriendCharacter('matt', 1000, 400)
+end
+
+local defaultHudX = 0
+local defaultHudY = 0
+
+local tvX = 0
+local tvY = 0
+
+local defaultWindowX = 0
+local defaultWindowY = 0
+
+local lastStep = 0
+
+function update (elapsed)
+
+end
+
+function beatHit (beat)    
+
+end
+
+function stepHit (step)
+
+end
